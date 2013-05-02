e6ee57abeb59f43844394155b62621a32faa5815
Mario Stoilov Stoilov <stoilov_mario@yahoo.com>
2013-05-02 17:15:56 +0300

    Updated tasks and class files

---
+++ Ballons pop/ConsoleInterface.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BalloonsPop
+{
+    class ConsoleInterface
+    {
+        //TODO: implement methods for the user to comunicate with the game trhough the console
+    }
+}
---
+++ Ballons pop/GameManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BalloonsPop
+{
+    class GameManager
+    {
+        //TODO: implement the methods for managing the game
+    }
+}
